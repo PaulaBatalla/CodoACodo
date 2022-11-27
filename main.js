63aaf7909c8e191dc8716bb38790e3377176867a
paulabatalla <mariapaulabatalla@hotmail.com>
2022-11-27 14:25:12 -0300

    Ultimos cambios

--- main.js
+++ main.js
@@ -3,30 +3,27 @@ function totalAPagar(cantTickets, categoria) {
     let total= cantTickets * valorTicket;
 
     switch(categoria) {
-        case "Estudiante":
-            total= total - (total * 80 / 100)
+        case "1":
+            total= total - (total * 80 / 100);
             break;
-        case "Trainee":
-            total= total - (total * 50 / 100)
+        case "2":
+            total= total - (total * 50 / 100);
             break;    
-        case "Junior":
-            total= total - (total * 15 / 100)
+        case "3":
+            total= total - (total * 15 / 100);
             break;
     }
     return total
 }
 
-let boton= document.getElementById("botonResumen")
+let boton= document.getElementById("botonResumen");
 
-boton.addEventListener("click", ()=>{
-    let cantidad= document.getElementById("cantidad").value 
-    let categoria= document.getElementById("categoria").value
+boton.addEventListener("click", ()=> {
+    let cantidad= document.getElementById("cantidad").value;
+    let categoria= document.getElementById("categoria").value;
 
-    let valor= totalAPagar(cantidad, categoria)
+    let valor= totalAPagar(cantidad, categoria);
 
     let divMensaje= document.getElementById("liveAlertPlaceholder")
     divMensaje.textContent= "Total a pagar: $" + valor
-    
-
-
 })
